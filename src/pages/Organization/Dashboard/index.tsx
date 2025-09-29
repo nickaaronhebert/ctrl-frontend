70e79c83f1805f9699d42ccafbad5c3202704580
rithik guleria <rithik.guleria@walkweltech.com>
2025-09-29 18:54:20 +0530

    "Added the changes for invitation flow"

--- src/pages/Organization/Dashboard/index.tsx
+++ src/pages/Organization/Dashboard/index.tsx
@@ -163,7 +163,7 @@ const OrganisationDashboard = () => {
             <h2 className="text-xl font-semibold text-dashboard-title mb-6">
               Recent Transmissions
             </h2>
-            <div className="w-full bg-white p-5 rounded-lg shadow-sm max-h-[500px]">
+            <div className="w-full bg-white p-5 rounded-lg shadow-sm max-h-[700px]">
               <DataTable table={table} className="p-5 bg-white" />
             </div>
           </div>
@@ -175,10 +175,10 @@ const OrganisationDashboard = () => {
           </div>
         </div>
       </div>
-      <p className="text-muted-foreground text-right">
+      {/* <p className="text-muted-foreground text-right">
         © 2025 CTRL. All Rights Reserved. Made with love by
         <span className="font-bold "> Telegra!</span>
-      </p>
+      </p> */}
     </>
   );
 };
